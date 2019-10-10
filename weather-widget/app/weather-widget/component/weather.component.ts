69c70c0e7c91b1e87cf78ea0838bd4d3ebf396c3
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-10 09:07:33 -0400

    Use model and modular services

--- weather-widget/app/weather-widget/component/weather.component.ts
+++ weather-widget/app/weather-widget/component/weather.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { WeatherService } from '../service/weather.service';
+import { Weather } from '../model/weather';
 
 @Component({
   moduleId: module.id,
@@ -9,19 +10,37 @@ import { WeatherService } from '../service/weather.service';
   styleUrls: ['weather.component.css'],
   providers: [WeatherService]
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
   pos: Position;
+  weatherData = new Weather(null, null, null, null, null);
 
-  constructor(private service: WeatherService) {
+  constructor(private service: WeatherService) {}
+
+  ngOnInit() {
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation() {
     this.service.getCurrentLocation()
       .subscribe(position => {
         this.pos = position;
-        this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
-          .subscribe(
-            weather => console.log(weather),
-            err => console.error(err)
-          );
+        this.getCurrentWeather();
       },
         err => console.error(err));
+  }
+
+  getCurrentWeather() {
+    this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
+      .subscribe(
+        ({ currently }) => {
+          this.weatherData.temp = currently.temperature;
+          this.weatherData.summary = currently.summary;
+          this.weatherData.wind = currently.windSpeed;
+          this.weatherData.humidity = currently.humidity;
+          this.weatherData.icon = currently.icon;
+          console.log(this.weatherData)
+        },
+        err => console.error(err)
+      );
   }
 }
